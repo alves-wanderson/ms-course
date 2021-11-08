a9532186d3143a784c168960db75cce224cceef7
Wanderson Alves <alves.wanderson@outlook.com>
2021-11-08 17:31:41 -0300

    Project hr-api-gateway-zuul created

---
+++ hr-api-gateway-zuul/src/test/java/com/wanderson/hrapigatewayzuulserver/HrApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanderson.hrapigatewayzuulserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
